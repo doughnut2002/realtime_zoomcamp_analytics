1b09532d5e84c47bee5a574ea202ec8c554078d1
Purnendu Shekhar Shukla <112604803+doughnut2002@users.noreply.github.com>
2024-02-15 13:14:43 +0000

    Done

--- docker/mage/Shekhar-IQ/data_exporters/api_to_gcs_1.py
+++ docker/mage/Shekhar-IQ/data_exporters/api_to_gcs_1.py
@@ -19,7 +19,7 @@ def export_data_to_google_cloud_storage(df: DataFrame, **kwargs) -> None:
     config_path = path.join(get_repo_path(), 'io_config.yaml')
     config_profile = 'default'
 
-    bucket_name = 'de-zoomcamp-project-data'
+    bucket_name = 'shekhar-iq-bucket'
     object_key = 'data/time_spent.parquet'
 
     GoogleCloudStorage.with_config(ConfigFileLoader(config_path, config_profile)).export(
